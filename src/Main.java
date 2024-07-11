83f19ce13375eb3a465708a158c3cbe8c7809e41
Vũ Đức Thành <52668021+thanhvd1501@users.noreply.github.com>
2024-07-11 21:05:18 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Changeeeeee");
+        System.out.println("Changeeeeee");
+        System.out.println("Changeeeeee");
+        System.out.println("Changeeeeee");
     }
 }
