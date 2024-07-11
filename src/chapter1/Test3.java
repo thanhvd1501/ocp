675b3a81126b44e89760fd33b254b57a693efd4a
Vũ Đức Thành <52668021+thanhvd1501@users.noreply.github.com>
2024-07-11 21:05:40 +0700

    Update Test3.java

--- src/chapter1/Test3.java
+++ src/chapter1/Test3.java
@@ -2,18 +2,5 @@ package chapter1;
 
 public class Test3 {
   // Test3
-  int a = 1;
-  int b = 1;
-  int d = 1;
-  int c = 1;
-  int test = 1;
-<<<<<<< HEAD
-  int test = 2;
-  int test = 3;
-  int c = 1;
-  int c = 1;
-  int c = 1;
-=======
->>>>>>> parent of b700002 (test)
 }
 
